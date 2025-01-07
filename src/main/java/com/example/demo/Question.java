78111e6a7f87aa1af191b8f50f35e4f737823475
notmybussiness <cuisine97@naver.com>
2025-01-07 16:09:33 +0900

    status

--- src/main/java/com/example/demo/Question.java
+++ src/main/java/com/example/demo/Question.java
@@ -1,4 +1,28 @@
 package com.example.demo;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany
+    private List<Answer> answerList;
 }
